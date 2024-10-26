87cf40750fc40fd25f0d847faad63f94217cb9a3
allseenn <allseen@live.ru>
2024-10-26 15:30:47 +0000

    05 State

--- www/src/App.js
+++ www/src/App.js
@@ -4,19 +4,33 @@ import Img from './components/Img'
 import logo from './img/01.png'
   
   class App extends React.Component {
+    constructor(props) { 
+        super(props)
+        this.state = { 
+            helpText: 'Help text',
+            userData: ''
+             }
+        this.mouseEnter = this.mouseEnter.bind(this)
+    }
     helpText = 'Enter your name!'
     render() {
       return (
         <div>
           <Header title="Шапка сайта"/>
-          <h1>{this.helpText}</h1>
-          <input placeholder={this.helpText} onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave} />
-          <p>{this.helpText === 'Enter your name' ? 'Yes' : 'No'}</p>
+          <h1>{this.state.helpText}</h1>
+          <h2>{this.state.userData}</h2>
+          <input placeholder={this.state.helpText} 
+          onChange={event => this.setState({userData: event.target.value})}
+          onMouseEnter={this.mouseEnter} 
+          onMouseLeave={this.mouseLeave} />
+          <p>{this.state.helpText === 'Enter your name' ? 'Yes' : 'No'}</p>
           <Img src={logo} alt="logo" />
         </div>
       )
     }
-    mouseEnter() { console.log('onmouseenter') }
+    mouseEnter() { 
+        this.setState({ helpText: 'Changed' }) 
+        console.log('onmouseenter') }
     mouseLeave() { console.log('onmouseleave') }
   }
 
